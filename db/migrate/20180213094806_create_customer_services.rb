08396e3f77bc51fc6f9181cad24c2610e2a5dca5
Sachin <sachindev108@gmail.com>
2018-02-14 15:52:53 +0530

    restructure db

--- db/migrate/20180213094806_create_customer_services.rb
+++ db/migrate/20180213094806_create_customer_services.rb
@@ -1,7 +1,7 @@
 class CreateCustomerServices < ActiveRecord::Migration[5.1]
   def change
     create_table :customer_services do |t|
-      t.integer :customer_number, index: true
+      t.string :customer_number, index: true
       t.text :comment
       t.timestamps
     end
