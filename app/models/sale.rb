0a7f552ccae908bd9e7eec246be0d6264c8be225
Sachin <sachindev108@gmail.com>
2018-02-15 18:07:22 +0530

    rubocop fixed

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -4,12 +4,12 @@ class Sale < ApplicationRecord # :nodoc:
   belongs_to :customer, foreign_key: 'customer_number',
                         primary_key: 'customer_number'
 
-  SOURCE_LIST = %w[instore e-commerce mobile]                   
+  SOURCE_LIST = %w[instore e-commerce mobile].freeze
 
   validates :item, :customer_number, :price, :date_of_transaction,
             :source, presence: true
 
   validates :price, numericality: true
   validates :source, inclusion: { in: SOURCE_LIST,
-                                  message: "The value: %{value} is not included in #{SOURCE_LIST}" }
+                                  message: "%<value>s not in #{SOURCE_LIST}" }
 end
