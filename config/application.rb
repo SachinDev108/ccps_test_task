d8f27f546a08b8b59f2ce3be2257fd4069d248df
Sachin <sachindev108@gmail.com>
2018-02-16 19:18:20 +0530

    set config to load file accroding to envs

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,8 @@ module CcpsTestTask
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.autoload_paths << Rails.root.join('lib')
+    load_path_strategy = Rails.env.production? ? :eager_load_paths : :autoload_paths
+    config.public_send(load_path_strategy) << Rails.root.join('lib')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
